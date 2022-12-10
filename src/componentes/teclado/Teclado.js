e772afc62db55948e3778cd69cc16d4f741b3d98
[mciannello] <[mciannello@gmail.com]>
2022-12-10 10:53:26 -0300

    Teclado COmpleto

--- src/componentes/teclado/Teclado.js
+++ src/componentes/teclado/Teclado.js
@@ -7,43 +7,60 @@ export default function Teclado ({clickHandler}){
 
   return (
     <div className="botonera">
-       <div className="fila">
-        <Button name="In" clickHandler={handleClick}/>
+        <div className="fila">
+        <Button name="x²" clickHandler={handleClick}/>
+        <Button name="x³" clickHandler={handleClick}/>
         <Button name="xⁿ" clickHandler={handleClick}/>
-        <Button name="e" clickHandler={handleClick}/>
-        <Button name="√" clickHandler={handleClick}/>
+        <Button name="log" clickHandler={handleClick}/>
+        <Button name="sen" clickHandler={handleClick}/>
+       
+      </div>
+       <div className="fila">
+         <Button name="√x" clickHandler={handleClick}/>
+         <Button name="³√x" clickHandler={handleClick}/>
+         <Button name="×√y" clickHandler={handleClick}/>
+        <Button name="cos" clickHandler={handleClick}/>
+        <Button name="tg" clickHandler={handleClick}/>
+        
 
       </div>
       <div className="fila">
-        <Button name="AC" clickHandler={handleClick}/>
-        <Button name="(" clickHandler={handleClick}/>
-        <Button name=")" clickHandler={handleClick}/>
-        <Button name="/" clickHandler={handleClick}/>
-
+      <Button name="In" clickHandler={handleClick}/>
+      <Button name="(" clickHandler={handleClick}/>
+      <Button name=")" clickHandler={handleClick}/>
+      <Button name="e" clickHandler={handleClick}/>
+      <Button name="!" clickHandler={handleClick}/>
       </div>
       <div className="fila">
         <Button name="7" clickHandler={handleClick}/>
         <Button name="8" clickHandler={handleClick}/>
         <Button name="9" clickHandler={handleClick}/>
-        <Button name="x" clickHandler={handleClick}/>
+        <Button name="DEL" clickHandler={handleClick}/>
+        <Button name="AC" clickHandler={handleClick}/>
       </div>
       <div className="fila">
         <Button name="4" clickHandler={handleClick}/>
         <Button name="5" clickHandler={handleClick}/>
         <Button name="6" clickHandler={handleClick}/>
-        <Button name="-" clickHandler={handleClick}/>
+        <Button name="x" clickHandler={handleClick}/>
+        <Button name="/" clickHandler={handleClick}/>
       </div>
       <div className="fila">
         <Button name="1" clickHandler={handleClick}/>
         <Button name="2" clickHandler={handleClick}/>
         <Button name="3" clickHandler={handleClick}/>
         <Button name="+" clickHandler={handleClick}/>
+        <Button name="-" clickHandler={handleClick}/>
+        
+        
       </div>
       <div className="fila">
         <Button  name="0" clickHandler={handleClick}/>
         <Button  name="." clickHandler={handleClick}/>
+        <Button name="+/-" clickHandler={handleClick}/>
         <Button name="=" clickHandler={handleClick}/>
-        <Button name="AC" clickHandler={handleClick}/>
+        <Button name="%" clickHandler={handleClick}/>
+        
       </div>
     </div>
   )
